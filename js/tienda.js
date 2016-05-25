54a2fdb678fc6aef55bfa0bf963ccd66008bea66
Ajani <ra1996@gmail.com>
2016-05-25 17:02:52 +0200

    feat: boton redesplegar 2

--- js/tienda.js
+++ js/tienda.js
@@ -303,7 +303,7 @@ function actualizarModalLogin(){
           $('#perfilBT').on('click',pantallaPerfil);
           $('#logoutBT').on('click',logout);
           if (sesion.empleado == "true") {
-              $('.login-modal-footer').append("<a class='btn btn-warning' href='backend.php'>Panel de Administración</a>");
+           
               $('.login-modal-footer').append("<a class='btn btn-warning' href='/backend'>Panel gvHIDRA</a>");
               $('.login-modal-footer').append("<a class='btn btn-danger' href='/server/deploy.php'>Redesplegar</a>");
 
